aad9330faaf1aaf29c6b873f73aea9bd7816e793
alfredlucero <alfred.lucero@ucla.edu>
2020-10-15 15:47:52 -0700

    Added hero detail component and wrapped up tutorial sort-of

--- angular/src/tour/tour.module.ts
+++ angular/src/tour/tour.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // ngModel comes from here
 
 import { TourComponent } from './tour.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 
 import { TourRoutingModule } from './tour-routing.module';
 
 @NgModule({
-  declarations: [TourComponent, HeroesComponent],
-  imports: [FormsModule, TourRoutingModule],
+  declarations: [TourComponent, HeroesComponent, HeroDetailComponent],
+  imports: [CommonModule, FormsModule, TourRoutingModule],
   exports: [TourComponent, HeroesComponent],
 })
 export class TourModule {}
