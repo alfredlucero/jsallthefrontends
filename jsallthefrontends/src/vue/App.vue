486e68ea5519ed6ec3840472e1e3dc20b781520f
alfredlucero <alfred.lucero@ucla.edu>
2020-10-21 00:27:35 -0700

    Added babel-loader cacheDirectory option

--- jsallthefrontends/src/vue/App.vue
+++ jsallthefrontends/src/vue/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
+<<<<<<< Updated upstream
     <h1>Hello from Vue!!</h1>
+=======
+    <h1>Hello from Vue!! Let's goooo</h1>
+    <HelloWorld />
+    <HelloWorld name="Alfred" />
+    <HelloWorldCode />
+>>>>>>> Stashed changes
   </div>
 </template>
 
