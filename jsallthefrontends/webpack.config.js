906b58bfd27dec207976b7da3ed41dc311cf2942
alfredlucero <alfred.lucero@ucla.edu>
2020-10-21 02:16:14 -0700

    Added base Atkinson Hyperlegible font and cleaned up base tailwind css styles

--- jsallthefrontends/webpack.config.js
+++ jsallthefrontends/webpack.config.js
@@ -40,6 +40,18 @@ module.exports = {
         exclude: /node_modules/,
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
