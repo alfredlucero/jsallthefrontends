272b2b23ec82cd5a668078508157960ea73d3958
alfredlucero <alfred.lucero@ucla.edu>
2020-10-26 01:30:04 -0700

    Integrated React home page with working code preview card for hello world

--- jsallthefrontends/src/react/components/CodePreviewCard.jsx
+++ jsallthefrontends/src/react/components/CodePreviewCard.jsx
@@ -1,6 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const otherFrontends;
+const otherFrontendsAvailableMap = {
+  svelte: {
+    previewLinkPrefix: "/svelte",
+    previewName: "Svelte",
+  },
+  vue: {
+    previewLinkPrefix: "/vue",
+    previewName: "Vue",
+  },
+};
+
+const PreviewLink = ({ link }) => {
+  return (
+    <Link
+      className="bg-blue-200 rounded px-4 py-2 text-lg font-semibold text-blue-700 hover:text-blue-900"
+      to={link}
+    >
+      <i className="fas fa-eye"></i> View
+    </Link>
+  );
+};
+
+const OtherFrontendsPreviewLink = ({ name, link }) => {
+  const otherFrontend = otherFrontendsAvailableMap[name];
+
+  if (otherFrontend) {
+    return (
+      <a
+        className="inline-block bg-gray-200 rounded px-3 py-1 text-sm font-semibold text-gray-700 hover:text-gray-900 mr-2 mb-2"
+        href={`${otherFrontend.previewLinkPrefix}${link}`}
+      >
+        {otherFrontend.previewName}
+      </a>
+    );
+  } else {
+    return null;
+  }
+};
 
 const CodePreviewCard = ({
   title,
@@ -9,18 +47,32 @@ const CodePreviewCard = ({
   otherFrontendsAvailable,
 }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{title}</div>
-        <p class="text-gray-700">{description}</p>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg transition ease-in-out duration-700 transform hover:scale-105">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700">{description}</p>
       </div>
-      <div class="px-6 pt-4 pb-2">
-        <a class="inline-block bg-gray-200 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-
-        </a>
+      <div className="flex justify-center mb-4 mx-4">
+        <PreviewLink link={previewLink} />
       </div>
+      {otherFrontendsAvailable.length > 0 && (
+        <div className="mt-6">
+          <p className="px-6 text-sm text-gray-600">
+            See it implemented in some other way!
+          </p>
+          <div className="px-6 py-2">
+            {otherFrontendsAvailable.map((otherFrontendAvailable, key) => (
+              <OtherFrontendsPreviewLink
+                name={otherFrontendAvailable}
+                link={previewLink}
+                key={key}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Card;
+export default CodePreviewCard;
