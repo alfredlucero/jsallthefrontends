5f50140ee5a6d08137522542d17a0de73dd12a4b
alfredlucero <alfred.lucero@ucla.edu>
2020-10-26 12:55:43 -0700

    Integrated Vue Helloworld example with CodePreview component

---
+++ jsallthefrontends/src/vue/components/CodePreview.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col lg:flex-row">
+    <div class="rounded-t-md lg:rounded-tl-md lg:rounded-bl-md lg:rounded-tr-none overflow-hidden bg-blue-100 p-4 flex-1 flex justify-center items-center">
+      <div><slot name="preview"></slot></div>
+    </div>
+    <div class="flex-1"><slot name="code"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CodePreview",
+};
+</script>
